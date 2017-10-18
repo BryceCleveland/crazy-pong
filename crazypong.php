1292ef9b036d537693cfe079ec7ee0eed75c6645
BryceCleveland <brycecleveland226@gmail.com>
2017-10-17 22:28:56 -0700

    Add files via upload

--- crazypong.php
+++ crazypong.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8">
 <title>Crazy Pong!</title>
 <link rel="stylesheet" href="css/crazypong.css" type="text/css" />
+<title>Bryce Cleveland - Crazy Pong Web Project</title>
 </head>
 
 <body>
@@ -17,11 +18,25 @@
     <div id="paddle4" class="paddle4"></div>
 </div>
 
-<div class="score-container">
-    <div id="greenScore"></div>
-    <div id="blueScore"></div>
-    <div id="redScore"></div>
-    <div id="orangeScore"></div>
+<div class="infoContainer">
+		<h2>Score:</h2>
+	    <div id="greenScore"></div>
+	    <div id="blueScore"></div>
+	    <div id="redScore"></div>
+	    <div id="orangeScore"></div>
+
+
+	<h2>AI Difficulty:</h2>
+	<form name="difficultyForm">
+		<input type="radio" name="difficulty" value="easy" checked> Easy<br>
+		<input type="radio" name="difficulty" value="medium"> Medium<br>
+		<input type="radio" name="difficulty" value="hard"> Hard<br>
+		<input type="radio" name="difficulty" value="impossible"> Impossible<br>
+	</form>
+	<button onclick="difficultyCheck()">Update Difficulty</button>
+		<div id="orangeSpeed"></div>
+		<div id="blueSpeed"></div>
+		<div id="greenSpeed"></div>
 </div>
 
 <script type="text/javascript" src="js/crazyPongEngine.js"></script>
